f2d15a4d6a4d4e053ef13787fa19124d10246e22
RPLN <rizqynugroho830@gmail.com>
2025-11-11 15:32:18 +0700

    materi

--- soal1projek.cpp
+++ soal1projek.cpp
@@ -1,3 +1,25 @@
 #include <iostream>
 using namespace std;
 
+int main(){
+    string namaadmin = "adnan";
+    string password = "admin123";
+    string namain,  passwordin, pembeli;
+    int jml;
+    do{
+        cout << "masukkan nama admin : ";
+        cin >> namain;
+        cout << "masukkan passowrd : ";
+        cin >> passwordin;
+    } while(passwordin != password || namain != namaadmin);
+
+    cout << "masukkan nama pembeli : ";
+    cin >> pembeli;
+    cout << "berapa total berat pakaian : ";
+    cin >> jml;
+    cout << "masukkan jenis layanan :" << endl;
+    cout << "1. 4 jam" << endl;
+    cout << "2. 7 jam" << endl;
+    cout << "3. 10 jam" << endl;
+    
+}
